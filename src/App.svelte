f26190f5ff1254000f80f801b00950bc539ea23c
Samuel S. Watson <samuel.s.watson@gmail.com>
2022-08-29 19:44:27 -0400

    shrink font

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,7 @@
 	import { onMount } from 'svelte';
 	import Board from './Board.svelte';
 	
-    import { flip } from 'svelte/animate';
-    import { dndzone } from 'svelte-dnd-action';
-    const flipDurationMs = 300;
-    function handleDndConsiderColumns(e) {
-        board = e.detail.items;
-    }
-    function handleDndFinalizeColumns(e) {
-        board = e.detail.items;
-    }
-    function handleDndConsiderCards(cid, e) {
-        const colIdx = board.findIndex(c => c.id === cid);
-        board[colIdx].items = e.detail.items;
-        board = [...board];
-    }
-    function handleDndFinalizeCards(cid, e) {
-        const colIdx = board.findIndex(c => c.id === cid);
-        board[colIdx].items = e.detail.items;
-        board = [...board];
-    }
-    function handleClick(e) {}
-	
 	let board = [];
-	let originalBoard = [];
 	
 	onMount(async () => {
 		const res = await fetch("https://sswatson.firebaseio.com/raidocs/board.json");
@@ -36,7 +14,6 @@
 				items: [],
 			}
 		});
-		originalBoard = board.slice();
 	})
 	
 	function addBoard() {
@@ -50,10 +27,6 @@
 				items: [],
 			}
 		];
-	}
-	
-	function deleteColumn(id) {
-		board = board.filter(item => item.id !== id);
 	}
 
 	async function writeBoardState() {
@@ -90,92 +63,4 @@
 	save
 </button>
 
-<section class="board" use:dndzone={{items:board, flipDurationMs, type:'columns'}} on:consider={handleDndConsiderColumns} on:finalize={handleDndFinalizeColumns}>
-    {#each board as column (column.id)}
-        <div class="column"
-             animate:flip="{{duration: flipDurationMs}}">
-            <div class="column-title">
-							<input 
-								value={column.name}
-								on:change={e => column.name = e.target.value}/>
-							{#if column.items.length === 0}
-								<button 
-									on:click={() => deleteColumn(column.id)}
-									class="delete-button">
-									[delete]
-								</button>
-							{/if}
-						</div>
-            <div class="column-content" 
-								 use:dndzone={{items:column.items, flipDurationMs}}
-                 on:consider={(e) => handleDndConsiderCards(column.id, e)} 
-								 on:finalize={(e) => handleDndFinalizeCards(column.id, e)}>
-                {#each column.items as item (item.id)}
-									<div class="card" 
-											 animate:flip="{{duration: flipDurationMs}}" on:click={handleClick}>
-										{ item.name }
-										<div class="description">
-											{ item.description }
-										</div>
-									</div>
-	              {/each}
-            </div>
-        </div>
-    {/each}
-</section>
-
-<style>
-		:root {
-			font-size: 10px;
-		}
-    .board {
-        height: calc(100vh - 100px);
-        width: 100%;
-        padding: 0.5em;
-        margin-bottom: 40px;
-    }
-    .column {
-        height: 100%;
-        width: 200px;
-        padding: 0.5em;
-        margin: 0.5em;
-        float: left;
-				border-radius: 4px;
-				background-color: #dbeafe;
-        /*Notice we make sure this container doesn't scroll so that the title stays on top and the dndzone inside is scrollable*/
-        overflow-y: hidden;
-    }
-    .column-content {
-        height: calc(100% - 40px);
-        /* Notice that the scroll container needs to be the dndzone if you want dragging near the edge to trigger scrolling */
-        overflow-y: scroll;
-			display: flex;
-			flex-direction: column;
-			gap: 12px;
-    }
-    .column-title {
-        margin-bottom: 1em;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .card {
-			width: 175px;
-			background-color: #2563eb;
-			color: white;
-			padding: 12px;
-			border-radius: 10px
-		}
-	
-		.description {
-			font-size: small;
-			color: #FAFAFA;
-			margin-top: 6px;
-		}
-	
-	.delete-button {
-		font-size: small;
-		background-color: transparent;
-		border: none;
-	}
-</style>
+<Board bind:columnItems={board}/>
--- src/Board.svelte
+++ src/Board.svelte
@@ -1,110 +1,125 @@
 <script>
-	export let onDelete;
-    import { flip } from 'svelte/animate';
-    import { dndzone } from 'svelte-dnd-action';
-    export let columnItems;
-    const flipDurationMs = 300;
-    function handleDndConsiderColumns(e) {
-        columnItems = e.detail.items;
-    }
-    function handleDndFinalizeColumns(e) {
-        columnItems = e.detail.items;
-    }
-    function handleDndConsiderCards(cid, e) {
-        const colIdx = columnItems.findIndex(c => c.id === cid);
-        columnItems[colIdx].items = e.detail.items;
-        columnItems = [...columnItems];
-    }
-    function handleDndFinalizeCards(cid, e) {
-        const colIdx = columnItems.findIndex(c => c.id === cid);
-        columnItems[colIdx].items = e.detail.items;
-        columnItems = [...columnItems];
-    }
-    function handleClick(e) {}
-</script>
-<style>
-    .board {
-        height: calc(100vh - 100px);
-        width: 100%;
-        padding: 0.5em;
-        margin-bottom: 40px;
-    }
-    .column {
-        height: 100%;
-        width: 250px;
-        padding: 0.5em;
-        margin: 0.5em;
-        float: left;
-				border-radius: 4px;
-				background-color: #dbeafe;
-        /*Notice we make sure this container doesn't scroll so that the title stays on top and the dndzone inside is scrollable*/
-        overflow-y: hidden;
-    }
-    .column-content {
-        height: calc(100% - 60px);
-        /* Notice that the scroll container needs to be the dndzone if you want dragging near the edge to trigger scrolling */
-        overflow-y: scroll;
-			display: flex;
-			flex-direction: column;
-			gap: 12px;
-    }
-    .column-title {
-        margin-bottom: 1em;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .card {
-			width: 225px;
-			background-color: #2563eb;
-			color: white;
-			padding: 12px;
-			border-radius: 10px
-		}
-	
-		.description {
-			font-size: small;
-			color: #FAFAFA;
-			margin-top: 6px;
-		}
-	
-	.delete-button {
-		font-size: small;
-		background-color: transparent;
-		border: none;
-	}
-</style>
+  export let columnItems;
+  import { flip } from "svelte/animate";
+  import { dndzone } from "svelte-dnd-action";
+  const flipDurationMs = 300;
+  function handleDndConsiderColumns(e) {
+    columnItems = e.detail.items;
+  }
+  function handleDndFinalizeColumns(e) {
+    columnItems = e.detail.items;
+  }
+  function handleDndConsiderCards(cid, e) {
+    const colIdx = columnItems.findIndex((c) => c.id === cid);
+    columnItems[colIdx].items = e.detail.items;
+    columnItems = [...columnItems];
+  }
+  function handleDndFinalizeCards(cid, e) {
+    const colIdx = columnItems.findIndex((c) => c.id === cid);
+    columnItems[colIdx].items = e.detail.items;
+    columnItems = [...columnItems];
+  }
+  function handleClick(e) {}
 
-<section class="board" use:dndzone={{items:columnItems, flipDurationMs, type:'columns'}} on:consider={handleDndConsiderColumns} on:finalize={handleDndFinalizeColumns}>
-    {#each columnItems as column (column.id)}
-        <div class="column"
-             animate:flip="{{duration: flipDurationMs}}">
-            <div class="column-title">
-							<input 
-								value={column.name}
-								on:change={e => column.name = e.target.value}/>
-							{#if column.items.length === 0}
-								<button 
-									on:click={() => onDelete(column.id)}
-									class="delete-button">
-									[delete]
-								</button>
-							{/if}
-						</div>
-            <div class="column-content" 
-								 use:dndzone={{items:column.items, flipDurationMs}}
-                 on:consider={(e) => handleDndConsiderCards(column.id, e)} 
-								 on:finalize={(e) => handleDndFinalizeCards(column.id, e)}>
-                {#each column.items as item (item.id)}
-									<div class="card" 
-											 animate:flip="{{duration: flipDurationMs}}" on:click={handleClick}>
-										{ item.name }
-										<div class="description">
-											{ item.description }
-										</div>
-									</div>
-	              {/each}
+  function deleteColumn(id) {
+    columnItems = columnItems.filter((item) => item.id !== id);
+  }
+</script>
+
+<section
+  class="board"
+  use:dndzone={{ items: columnItems, flipDurationMs, type: "columns" }}
+  on:consider={handleDndConsiderColumns}
+  on:finalize={handleDndFinalizeColumns}
+>
+  {#each columnItems as column (column.id)}
+    <div class="column" animate:flip={{ duration: flipDurationMs }}>
+      <div class="column-title">
+        <input
+          value={column.name}
+          on:change={(e) => (column.name = e.target.value)}
+        />
+        {#if column.items.length === 0}
+          <button
+            on:click={() => deleteColumn(column.id)}
+            class="delete-button"
+          >
+            [delete]
+          </button>
+        {/if}
+      </div>
+      <div
+        class="column-content"
+        use:dndzone={{ items: column.items, flipDurationMs }}
+        on:consider={(e) => handleDndConsiderCards(column.id, e)}
+        on:finalize={(e) => handleDndFinalizeCards(column.id, e)}
+      >
+        {#each column.items as item (item.id)}
+          <div
+            class="card"
+            animate:flip={{ duration: flipDurationMs }}
+            on:click={handleClick}
+          >
+            {item.name}
+            <div class="description">
+              {item.description}
             </div>
-        </div>
-    {/each}
+          </div>
+        {/each}
+      </div>
+    </div>
+  {/each}
 </section>
+
+<style>
+  .board {
+    height: calc(100vh - 70px);
+    width: 100%;
+    padding: 0.5em;
+    margin-bottom: 40px;
+  }
+  .column {
+    height: 100%;
+    width: 200px;
+    padding: 0.5em;
+    margin: 0.5em;
+    float: left;
+    border-radius: 4px;
+    background-color: #dbeafe;
+    /*Notice we make sure this container doesn't scroll so that the title stays on top and the dndzone inside is scrollable*/
+    overflow-y: hidden;
+  }
+  .column-content {
+    height: calc(100% - 40px);
+    /* Notice that the scroll container needs to be the dndzone if you want dragging near the edge to trigger scrolling */
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
+  .column-title {
+    margin-bottom: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .card {
+    width: 175px;
+    background-color: #2563eb;
+    color: white;
+    padding: 12px;
+    border-radius: 10px;
+  }
+
+  .description {
+    font-size: small;
+    color: #fafafa;
+    margin-top: 6px;
+  }
+
+  .delete-button {
+    font-size: small;
+    background-color: transparent;
+    border: none;
+  }
+</style>
